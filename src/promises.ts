be957392ae34a75142c3de7625278723c1b36927
Alberto Benatti <albi.benni8@gmail.com>
2023-01-23 20:58:13 +0100

    prep node server

---
+++ src/promises.ts
@@ -0,0 +1,20 @@
+
+const fetchData = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Done")
+        }, 1500);
+        reject("Rejected");
+    });
+    return promise;
+}
+
+setTimeout(() => {
+    console.log("Timer done!");
+    fetchData().then(text => {
+        console.log(text);
+    })
+}, 2000);
+
+console.log("main");
+console.log("Running");
