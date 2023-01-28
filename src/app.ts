499c2650bc1ada631866d3904a75f29851a69315
Alberto Benatti <albi.benni8@gmail.com>
2023-01-28 16:43:29 +0100

    redirect and writes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import * as http from 'http';
+import * as fs from 'fs';
 
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
+    const url = req.url;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title> Enter Message </title></head>');
+        res.write('<body><form action="/message" method="POST" >' +
+            '<input type="text" name ="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end('Hello World\n');
+    }
+    if (url === '/message' && req.method === 'POST') {
+        fs.writeFileSync('message.text', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title> My First Page </title></head>');
     res.write('<body><h1>Hello from my Node server learn project!</h1></body>');
