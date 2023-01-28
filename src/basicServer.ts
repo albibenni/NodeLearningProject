2b7e9a1880d6e3dcf688119f20ba71da73d83a0c
Alberto Benatti <albi.benni8@gmail.com>
2023-01-28 15:45:20 +0100

    node server write html

--- src/basicServer.ts
+++ src/basicServer.ts
@@ -3,12 +3,9 @@ import * as http from 'http';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers);
+    console.log(req);
     res.statusCode = 200;
-    res.write('<html>');
-    res.write('<head><title> My First Page <title><head>');
-    res.write('<body><h1>Hello from my Node server learn project!<h1><body>');
-    res.write('<html>');
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World\n');
 });
 
