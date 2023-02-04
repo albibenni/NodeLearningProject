cdafb107a2cc4f865eb9229d068a09a53d02a3e1
Alberto Benatti <albi.benni8@gmail.com>
2023-02-04 19:26:47 +0100

    eslint setup and prettier. Still not working on save

--- NodeCourse/src/app.ts
+++ NodeCourse/src/app.ts
@@ -1,8 +1,8 @@
 import * as http from 'http';
-import {reqHandler} from './routes';
+import { reqHandler } from './routes';
 
 const port = 3000;
 const server = http.createServer(reqHandler);
 server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
